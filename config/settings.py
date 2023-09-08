877052974160656462bc5dfd96590cf3262c0cc7
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-08 13:57:27 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,6 @@
+# config/settings.py
+
+DISCORD_API_KEY = 'YOUR_DISCORD_API_KEY'
+CHANNEL_ID = 'YOUR_DISCORD_CHANNEL_ID'
+REGEX_PATTERN = r'YOUR_REGEX_PATTERN'  # Placeholder for command regex pattern
  
