b4b128e9683eaa9b06a876bfad9f5eaa9760f40f
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-08 14:04:01 +0500

    Update user_management.py

--- modules/user_management.py
+++ modules/user_management.py
@@ -1,1 +1,65 @@
+# modules/user_management.py
+
+import discord
+from config.settings import DISCORD_API_KEY, CHANNEL_ID
+
+client = discord.Client()
+
+class UserManagement:
+
+    def __init__(self, token):
+        self.token = token
+        self.client = discord.Client()
+        
+    async def add_user_to_channel(self, user_id, channel_id):
+        """
+        Add a user to a specific Discord channel.
+        """
+        user = self.client.get_user(user_id)
+        if not user:
+            print(f"User with ID {user_id} not found.")
+            return
+
+        channel = self.client.get_channel(channel_id)
+        if not channel:
+            print(f"Channel with ID {channel_id} not found.")
+            return
+
+        # Permissions logic here
+        # As mentioned earlier, the Discord API doesn't directly support adding a user to a channel.
+        # This would typically be managed via roles or other means.
+        # The below is a placeholder that sets read permissions for the user for the channel.
+        permissions = discord.PermissionOverwrite(read_messages=True)
+        await channel.set_permissions(user, overwrite=permissions)
+        print(f"User {user.name} has been added to the channel {channel.name}")
+
+    async def add_multiple_users_to_channel(self, user_ids, channel_id):
+        """
+        Add multiple users to a specific Discord channel.
+        """
+        for user_id in user_ids:
+            await self.add_user_to_channel(user_id, channel_id)
+
+    async def create_and_invite_to_channel(self, user_ids, channel_name):
+        """
+        Create a new channel and invite specified users.
+        """
+        guild = discord.utils.get(self.client.guilds, name="YOUR_GUILD_NAME")  # Replace with your guild/server name
+        channel = await guild.create_text_channel(channel_name)
+
+        for user_id in user_ids:
+            await self.add_user_to_channel(user_id, channel.id)
+
+    def run(self):
+        @self.client.event
+        async def on_ready():
+            print(f'Bot has logged in as {client.user}')
+
+        self.client.run(self.token)
+
+if __name__ == "__main__":
+    manager = UserManagement(DISCORD_API_KEY)
+    # Example usage (you'd replace with actual user IDs and desired channel name):
+    # await manager.create_and_invite_to_channel(['1234567890', '0987654321'], 'new_channel_name')
+    manager.run()
  
