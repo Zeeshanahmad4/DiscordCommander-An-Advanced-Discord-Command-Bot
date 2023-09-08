9c5b5b42f8dfcfcc911ce3b844c56a58fa5c8bd0
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-09-08 14:02:07 +0500

    Update discord_utils.py

--- utils/discord_utils.py
+++ utils/discord_utils.py
@@ -1,1 +1,16 @@
+# utils/discord_utils.py
+
+import discord
+
+def initialize_bot(token):
+    """
+    Initialize and run the Discord bot.
+    """
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'Bot has logged in as {client.user}')
+
+    client.run(token)
  
